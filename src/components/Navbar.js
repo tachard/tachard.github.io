b41b554832bd1daf6a7857c3b8202ffb89965d28
tachard <tachard@ensc.fr>
2023-08-20 14:59:24 +0200

    Added navbar, pages and navigation

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const NavBar = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand>Thomas Achard</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Work</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
---
+++ src/pages/workDetail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WorkDetail = () => {
+  return (
+    <div>
+      <h1>Detail about one particular work.</h1>
+    </div>
+  );
+};
+
+export default WorkDetail;
