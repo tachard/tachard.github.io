754da19521e264f82987afd1ff7a4d42ee314660
tachard <94848645+tachard@users.noreply.github.com>
2025-02-10 15:27:35 +0000

    Set up

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,19 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  core: {
-    disableWhatsNewNotifications: true,
-    disableTelemetry: true,
-    enableCrashReports: false,
-  },
-  stories: ['../src/**/*.mdx', '../src/**/*.story.@(js|jsx|ts|tsx)'],
-  addons: ['storybook-dark-mode'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
+  core:{
+    builder: '@storybook/builder-vite',
+  },
 };
-
 export default config;
