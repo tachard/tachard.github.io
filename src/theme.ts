077848ce18d77643937a7ab7a1a5bed8ee8fa22f
tachard <94848645+tachard@users.noreply.github.com>
2025-02-10 15:52:12 +0000

    theme update

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,8 @@
 import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
-  /** Put your mantine theme override here */
+  primaryColor:"green",
+  primaryShade:9,
+  defaultRadius:"sm",
+  
 });
