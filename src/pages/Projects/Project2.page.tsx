61c3acab39fa2191ad9143ea8a7181f031450ab0
tachard <94848645+tachard@users.noreply.github.com>
2025-02-24 15:28:37 +0000

    Project 2 writing part 2

--- src/pages/Projects/Project2.page.tsx
+++ src/pages/Projects/Project2.page.tsx
@@ -26,22 +26,65 @@ export function Project2(){
                         <ListItem>L'interface existante : écrans, manuels,</ListItem>
                         <ListItem>Les attentes clients sous forme de wireframe PowerPoint et Figma,</ListItem>
                         <ListItem>Les spécifications du nouvel appareil</ListItem>
-                    </List>
-                    L'interface existante et les wireframes ont permis de fixer l'user flow général et d'avoir une première idée des écrans.<br/>
+                    </List> <br/>
+                    L'interface existante et les wireframes ont permis de fixer l'user flow général et d'avoir une première idée des écrans, et de l'ordre à avoir. Il s'agit aussi de s'assurer une certaine familiarité pour les utilisateurs de l'appareil initial.<br/>
                     Les spécifications du nouvel appareil ont permis de donner des premières règles de conception :
                     <List withPadding>
-                        <ListItem>Il s'agit d'un écran de 15 pouces en résolution 1920x1080,</ListItem>
+                        <ListItem>Il s'agit d'un écran tactile de 15 pouces en résolution 1920x1080,</ListItem>
                         <ListItem>L'écran est à 1,70m de haut, ce qui induit que les interactions doivent être plutôt vers le bas pour accommoder les plus petits utilisateurs,</ListItem>
                         <ListItem>
                             Il y a 2 modes de lecture : un premier à 40cm pour lire des informations détaillées et interagir, et un second à 4m pour recueillir de l'information.
                             Donc il faut au moins une police très grande (au moins 140pt d'après{" "} <Anchor href="https://webstore.ansi.org/standards/aami/ansiaamihe752009r2018" underline="hover">l'ANSI/AAMI HE75</Anchor>).
                         </ListItem>
                     </List>
-                </Text>
-                
+                </Text>  
             </Group>
             <BetterTitle align="flex-start" mb={20} pb={5} order={4} size="h3">Revenir au niveau de l'existant</BetterTitle>
+            <Group mb={20}>
+                <Grid className={classes.grid} gutter="xl">
+                    <Grid.Col span={{xs:12, sm:8}}>
+                        <Text>
+                            Dans l'idée d'une non-regression et de garder une familiarité, la tâche de fond de ce maquettage était de s'assurer de la présence des mêmes fonctionnalités que dans l'ancienne version.
+                            Pour le fond, il fallait garder un user flow similaire (voir l'image), afin de garder la logique métier cohérente.
+                            Pour les interactions, celles-ci devaient changer car sur l'ancien appareil, l'écran n'était pas tactile donc tout était géré par un bouton rotatif poussable.
+                            Ainsi, pour les entrées numériques, il a fallu ajouter des claviers, selon ce qui pouvait être rentré ou affiché. <br/>
+                        </Text>
+                    </Grid.Col>
+                    <Grid.Col span={{xs:12, sm:4}}>
+                        <Image src="/src/assets/Maquettage_UserFlow.png" />
+                    </Grid.Col>
+                </Grid>
+                <Text>
+                    Pour la forme, l'interface existante avait un certain style, utilisant des tuiles surélevées pour faire ressortir des briques d'information.
+                    Il a été décidé de garder cette direction artistique et même de la pousser plus loin. La limite de l'interface actuelle et des demandes client était que l'espace d'écran
+                    était perçu uniquement en 2D avec des zones prédéfinies. Une zone étant prévue pour la gestion des alarmes, l'interface était soit vide, soit remplie.
+                    Or, la direction artistique, utilisant <Anchor href="https://m2.material.io/design/environment/elevation.html" underline="hover">l'élévation</Anchor>,
+                    permet plus facilement de mettre en place cette troisième dimension. Sur cette dimension plus proche de l'utilisateur, on a mis la gestion des alarmes, pour qu'elle ressorte mieux pour l'utilisateur,
+                    tout en garantissant des possibilités d'interaction ailleurs, ce que ne permettrait pas un scrim.         
+                </Text>
+            </Group>
             <BetterTitle align="flex-start" mb={20} pb={5} order={4} size="h3">Introduire de la nouveauté</BetterTitle>
+            <Group mb={20}>
+                <Grid className={classes.grid} gutter="xl">
+                    <Grid.Col span={{xs:12, sm:8}}>
+                        <Text>
+                            En même temps que le point précédent, l'objectif était aussi de challenger certains choix de conceptions faits pour la première version.
+                            Ces choix de conception allient du user flow lui-même à des détails, néanmoins importants, comme certains choix icônographiques (voir plus bas).
+                            Une étape du user flow remis en cause par exemple est la présence d'un écran à faible valeur ajoutée en phase de préparation. Cet écran dans la version originale était présenté comme suit :
+                            Vous arrivez obligatoirement à cet écran en fin de préparation et vous avez 3 options A B et C. Vous êtes libre de cliquer sur l'une des 3 options. Toutefois il était écrit que
+                            vous deviez faire l'option B avant C. La correction évidente a été de changer ce moment en fusionnant cet écran avec le précédent et en ne proposant que les options A et B, B débouchant sur C.
+                        </Text>
+                    </Grid.Col>
+                    <Grid.Col span={{xs:12, sm:4}}>
+                        <Image src="/src/assets/Maquette_UserFlowDetail.png" />
+                    </Grid.Col>
+                </Grid>
+                <Text>
+                    Il y a aussi eu des ajouts. Un exemple de fonctionnalité ajoutée est la possibilité d'avoir des favoris. De là, plusieurs questions se sont posées immédiatement&nbsp;: 
+                    ce qu'englobe un favori, combien en afficher, comment en ajouter, modifier ou supprimer. Ces questions ont été répondues en imaginant 
+                    l'appareil dans un contexte d'utilisation global : besoins utilisateurs, différents profils d'utilisateurs, accès privilégiés ou non ...
+                </Text>
+            </Group>
             <BetterTitle align="flex-start" mb={30} order={3} size="h2">Tester la maquette</BetterTitle>
             <BetterTitle align="flex-start" mb={20} pb={5} order={4} size="h3">Préparation</BetterTitle>
             <BetterTitle align="flex-start" mb={20} pb={5} order={4} size="h3">Passation</BetterTitle>
